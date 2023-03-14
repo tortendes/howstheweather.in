7caba8d537b3d7ceb024319e7d6fe239915f4d6c
Jarelle Emmanuel Payad Andulan <me@torten.xyz>
2023-03-14 22:26:13 +0800

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Lookup from './lib/Lookup.svelte'
+  import { data } from './lib/store';
+
+  let weatherData;
+
+  data.subscribe(val => {
+    weatherData = val
+  })
+</script>
+
+<main>
+  <Lookup />
+  {JSON.stringify($data)}
+</main>
---
+++ src/lib/Lookup.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import { data } from "./store";
+    let lookup: string;
+
+    function searchData() {
+        data.set({
+            'location': lookup
+        })
+    }
+</script>
+
+<div>
+    <div class="container">
+        <h1 class="title">How's<br>the<br>weather?</h1>
+        <form class="form-body" on:submit|preventDefault={searchData}>
+            <input type="text" placeholder="Enter LOCATION then press RETURN" bind:value={lookup}>
+            <button type="submit">RETURN</button>
+        </form>
+    </div>
+</div>
+
+<style>
+    .container {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    h1.title {
+        font-family: 'Bebas Neue', Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+        font-size: 3rem;
+    }
+    form.form-body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    input[type=text] {
+        background: transparent;
+        color: #fff;
+        padding: 1em 0.75em;
+        border: none;
+        min-width: 30vw;
+        border: rgb(232,81,18) solid 3px;
+        border-right: none;
+    }
+    button {
+        background-color: rgb(232,81,18);
+        border: none;
+        height: 2.9rem;
+        padding: 1.3em 3em;
+        cursor: pointer;
+    }
+</style>
